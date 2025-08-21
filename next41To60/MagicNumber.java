d4462dfbfc32d555cc42d45c0eb44bfffab39087
Parmeshwar Gurlewad <114818920+param7666@users.noreply.github.com>
2025-08-22 00:42:29 +0530

    Add files via upload

--- next41To60/MagicNumber.java
+++ next41To60/MagicNumber.java
@@ -2,10 +2,31 @@ package next41To60;
 
 /*
 39.	Check Magic Number Problem: Write a program to check if a number is magic. 
-Logic: Repeatedly sum digits until single digit; if 1, it's magic. Test Case: Input: number = 19 Output: 19 is a magic number 
+Logic: Repeatedly sum digits until single digit; if 1, it's magic. 
+Test Case: Input: number = 19 Output: 19 is a magic number 
 
  */
 
 public class MagicNumber {
 
+	protected static int sumOfDigit(int num) {
+		int sum=0;
+		while(num>0) {
+			int digit=num%10;
+			sum+=digit;
+			num/=10;
+		}
+		return sum;
+	}
+	
+	public static void isMagic(int num) {
+		while(num>=10) {
+			num=sumOfDigit(num);
+		}
+		boolean result=num==1;
+		System.out.println(result?"Number is magic":"number is not magic");
+	}
+	public static void main(String[] args) {
+		isMagic(19);
+	}
 }
